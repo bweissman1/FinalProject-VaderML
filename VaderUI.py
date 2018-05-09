f2a72a1745aecdc9a8e6733f51a63337347af733
Ben Weissman <32114969+bweissman1@users.noreply.github.com>
2018-05-08 23:09:10 -0400

    Update VaderUI.py

--- VaderUI.py
+++ VaderUI.py
@@ -34,4 +34,6 @@ def resources():
 
 
 if __name__ == "__main__":
-    app.run(debug=False, use_reloader=False)
+    HOST = '0.0.0.0' if 'PORT' in os.environ else '127.0.0.1'
+    PORT = int(os.environ.get('PORT', 5000))
+    app.run(host=HOST, port=PORT)
